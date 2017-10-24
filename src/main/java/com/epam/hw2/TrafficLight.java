6ab14270b95aa689d19f7e6b7bc6b1ae55c25713
fogelmariya <fogelmariya@gmail.com>
2017-10-24 18:24:40 +0300

    corrections

--- src/main/java/com/epam/hw2/TrafficLight.java
+++ src/main/java/com/epam/hw2/TrafficLight.java
@@ -18,26 +18,31 @@ public class TrafficLight {
      * @param activeTime - what time Traffic Light is working
      * @return String of what color is now
      */
-    public static String returnLight(int activeTime) {
+    public String returnLight(int activeTime) {
+        if (activeTime < 0) {
+            throw new IllegalArgumentException("Time should pe positive" + activeTime);
+        }
         int restOfTimeFromPeriod = activeTime % 10;
         if (restOfTimeFromPeriod >= 0 && restOfTimeFromPeriod < 2) {
             return " red ";
-        } else if (restOfTimeFromPeriod >= 2 && restOfTimeFromPeriod < 5) {
+        } else if (restOfTimeFromPeriod < 5) {
             return " yellow ";
-        } else {
+        } else if (restOfTimeFromPeriod < 10){
             return " green ";
         }
+        throw new RuntimeException("Something is wrong" + activeTime);
     }
 
     public static void main(String[] args) throws IOException {
-
+        TrafficLight trafficLight = new TrafficLight();
+        int activeTime;
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Input minutes of traffic light's work: ");
         String param = reader.readLine();
-        int activeTime = Integer.parseInt(param);
+        activeTime = Integer.parseInt(param);
 
         if (activeTime >= 0) {
-            System.out.println("Now is" + returnLight(activeTime) + "light");
+            System.out.println("Now is" + trafficLight.returnLight(activeTime) + "light");
         } else {
             System.out.println("Wrong time");
         }
--- src/test/java/com/epam/hw2/TrafficLightTest.java
+++ src/test/java/com/epam/hw2/TrafficLightTest.java
@@ -1,43 +1,26 @@
 package com.epam.hw2;
-/**
- * Test for method ReturnLight, which output color of traffic light, getting minutes of it's working
- */
 
-import org.junit.After;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class TrafficLightTest {
-    TrafficLight trafficLight = new TrafficLight();
+    private TrafficLight trafficLight;
 
     @Before
-    public void before() {
-        System.out.println("New Test");
+    public void setup() {
+        trafficLight = new TrafficLight();
     }
+    /**
+     * Test for method ReturnLight, which output color of traffic light, getting minutes of it's working
+     */
 
     @Test
-    public void testFor1MinuteRedColor() {
+    public void testReturnLight() {
         Assert.assertEquals("wrong", " red ", trafficLight.returnLight(1));
-    }
-
-    @Test
-    public void testFor4MinuteYellowColor() {
         Assert.assertEquals("wrong", " yellow ", trafficLight.returnLight(4));
-    }
-
-    @Test
-    public void testFor7MinuteGreenColor() {
         Assert.assertEquals("wrong", " green ", trafficLight.returnLight(7));
-    }
-
-    @Test
-    public void testFor63MinuteYellowColor() {
         Assert.assertEquals("wrong", " yellow ", trafficLight.returnLight(63));
-    }
-
-    @After
-    public void after() {
-        System.out.println("End of the test");
     }
 }
